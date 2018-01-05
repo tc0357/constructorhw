df767c8d88ac9a4195b4eced34528cab2cb55861
tc0357 <30632197+tc0357@users.noreply.github.com>
2018-01-04 20:41:11 -0800

    Create randomFood.js

---
+++ randomFood.js
@@ -0,0 +1,5 @@
+//list of words to guess.
+//Need to figure out what the \n thing is, how to create empty letter holders?
+exports.newFood = {
+    foodList:["Pizza", "Burrito", "Chicken Wings"]
+  };
